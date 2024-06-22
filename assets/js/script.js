d8a89380878dc81662450c7c46091251f4c7c878
Arif Ahmed <borndaee.wif@gmail.com>
2024-06-22 14:39:10 +0600

    skills and project

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,8 +67,6 @@ document.addEventListener('visibilitychange',
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
     });
-
-8
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
     strings: ["Industrial Design", "Industrial Automation", "Internet of Things", "TVET Training", "Project Planning"],
@@ -79,258 +77,96 @@ var typed = new Typed(".typing-text", {
 });
 // <!-- typed js effect ends -->
 
-const skillsData = [
-    {
-      name: "SolidWorks",
-      icon: "https://img.icons8.com/?size=100&id=62397&format=png&color=000000",
-      category: "CAD Software"
-    },
-    {
-      name: "Onshape",
-      icon: "https://play-lh.googleusercontent.com/yAS9WJJnjlCx77RxIvJSssrixhCdUxnBlM3CuPnQpl8QI3Ez19KreBL4xREc1gtmK_Y",
-      category: "CAD Software"
-    },
-    {
-      name: "Fusion 360",
-      icon: "https://www.softexia.com/wp-content/uploads/2024/04/Autodesk_Fusion_360.webp",
-      category: "CAD Software"
-    },
-    {
-      name: "AutoCAD",
-      icon: "https://logos-world.net/wp-content/uploads/2020/12/Autocad-Logo.png",
-      category: "CAD Software"
-    },
-    {
-      name: "Autodesk Inventor",
-      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPWa67bjrJIYCX67ffP36BlY09SpFsbsSSmg&s",
-      category: "CAD Software"
-    },
-    {
-      name: "Catia",
-      icon: "https://upload.wikimedia.org/wikipedia/commons/6/60/DS-CATIA-Logo.png",
-      category: "CAD Software"
-    },
-    {
-      name: "FreeCAD",
-      icon: "https://e7.pngegg.com/pngimages/615/745/png-clipart-freecad-computer-aided-design-open-source-software-3d-modeling-software-design-blue-angle.png",
-      category: "CAD Software"
-    },
-    {
-      name: "MasterCAM",
-      icon: "https://allvectorlogo.com/img/2017/07/mastercam-logo.png",
-      category: "CAD Software"
-    },
-    {
-      name: "ArtCAM",
-      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToMgjw-7VBgpkxB6xVHUXznrxWymOg2zJChg&s",
-      category: "CAD Software"
-    },
-    {
-      name: "Geomagic Design X",
-      icon: "https://gdm-catalog-fmapi-prod.imgix.net/ProductLogo/50803a25-8e13-4f65-8c2e-f81d1296f633.png",
-      category: "CAD Software"
-    },
-    {
-      name: "C/C++",
-      icon: "https://img.icons8.com/color/48/000000/c-plus-plus-logo.png",
-      category: "Programming Language"
-    },
-    {
-      name: "Python",
-      icon: "https://img.icons8.com/color/48/000000/python--v1.png",
-      category: "Programming Language"
-    },
-    {
-      name: "JavaScript",
-      icon: "https://img.icons8.com/color/48/000000/javascript--v1.png",
-      category: "Programming Language"
-    },
-    {
-      name: "NodeJS",
-      icon: "https://img.icons8.com/color/48/000000/nodejs.png",
-      category: "Programming Language"
-    },
-    {
-      name: "PostgreSQL",
-      icon: "https://img.icons8.com/color/48/000000/postgreesql.png",
-      category: "Programming Language"
-    },
-    {
-      name: "DigitalOcean",
-      icon: "https://img.icons8.com/ios-filled/48/0080FF/digitalocean.png",
-      category: "Programming Language"
-    },
-    {
-      name: "GitHub",
-      icon: "https://img.icons8.com/glyph-neue/48/ffffff/github.png",
-      category: "Programming Language"
-    },
-    {
-      name: "HTML5",
-      icon: "https://img.icons8.com/color/48/000000/html-5--v1.png",
-      category: "Programming Language"
-    },
-    {
-      name: "CSS3",
-      icon: "https://img.icons8.com/color/48/000000/css3.png",
-      category: "Programming Language"
-    },
-    ,
-    {
-      name: "Linux",
-      icon: "https://cdn-icons-png.flaticon.com/512/518/518713.png",
-      category: "Programming Language"
-    },
-    {
-      name: "Raspberry Pi",
-      icon: "https://miro.medium.com/v2/resize:fit:1200/0*EonQaU90v2fwXO3c.png",
-      category: "Automation and IoT"
-    },
-    {
-      name: "Siemens PLC",
-      icon: "https://ngocautomation.com/wp-content/uploads/2020/08/TIA-V16-1.png",
-      category: "Automation and IoT"
-    },
-    {
-      name: "SCADA",
-      icon: "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_d19102425cd16dd7ea2e286d8f3d694a/simatic-wincc.jpg",
-      category: "Automation and IoT"
-    },
-    {
-      name: "Mitsubishi PLC",
-      icon: "https://vattutudonghoa.com/content/images/thumbs/0017273_software_gx_works_3_ver_1038q.png",
-      category: "Automation and IoT"
-    },
-    {
-      name: "Arduino",
-      icon: "https://icon-icons.com/icons2/159/PNG/256/arduino_22429.png",
-      category: "Automation and IoT "
-    },
-    {
-      name: "Node-Red",
-      icon: "https://logowik.com/content/uploads/images/node-red4832.logowik.com.webp",
-      category: "Automation and IoT "
-    },
-    {
-      name: "ThingsBoard",
-      icon: "https://asset.brandfetch.io/id8ftKrGt3/idc1SXKXki.jpeg",
-      category: "Automation and IoT "
-    },
-    {
-      name: "MQTT",
-      icon: "https://mqtt.org/assets/downloads/mqtt-ver.png",
-      category: "Automation and IoT "
-    },
-    {
-      name: "Ansys",
-      icon: "https://e7.pngegg.com/pngimages/51/79/png-clipart-ansys-logo-nasdaq-anss-engineering-geometry-background-company-text.png",
-      category: "Research and Simulation"
-    },
-    {
-      name: "MATLAB",
-      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc36bSKd51qFqjmBk_cDA3eNAeYF2Dtc_fWQ&s",
-      category: "Research and Simulation"
-    },
-    {
-      name: "CFTurbo",
-      icon: "https://image.pitchbook.com/YQLCAPVm9g8Vi7fScFYsr0oqxCp1498833861029_200x200",
-      category: "Research and Simulation"
-    },
-    {
-      name: "ROS2",
-      icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ72zhKupIgXa2wpEOH35TkbgDRkqGfgiCM1Q&s",
-      category: "Research and Simulation"
-    },
-    {
-      name: "Gazebo",
-      icon: "https://gazebosim.org/assets/images/logos/gazebo_vert_pos.svg",
-      category: "Research and Simulation"
-    }
-
-  ];
-  
-  function showSkills(skills) {
-    let skillsContainer = document.getElementById("skillsContainer");
-    let skillHTML = "";
-    skills.forEach(skill => {
-      skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
+// fetch json 
+async function fetchData(type = "skills") {
+  let response;
+  if (type === "skills") {
+    response = await fetch("skills.json");
+  } else if (type === "projects") {
+    response = await fetch("./projects/projects.json");
+  } else {
+    throw new Error("Invalid type specified for fetchData function.");
   }
+  const data = await response.json();
+  return data;
+}
+
   
-  function filterSkills(category) {
-    let filteredSkills = skillsData;
+function showSkills(skills) {
+  let skillsContainer = document.getElementById("skillsContainer");
+  let skillHTML = "";
+  skills.forEach(skill => {
+    skillHTML += `
+      <div class="bar">
+        <div class="info">
+          <img src="${skill.icon}" alt="${skill.name}" />
+          <span>${skill.name}</span>
+        </div>
+      </div>`;
+  });
+  skillsContainer.innerHTML = skillHTML;
+}
+
+function filterSkills(category) {
+  fetchData("skills").then(data => {
+    let filteredSkills = data;
     if (category !== 'all') {
-      filteredSkills = skillsData.filter(skill => skill.category === category);
+      filteredSkills = data.filter(skill => skill.category === category);
     }
     showSkills(filteredSkills);
-  }
+  });
+}
+
   
   // Show all skills on page load
   document.addEventListener("DOMContentLoaded", function() {
-    showSkills(skillsData);
+    fetchData("skills").then(data => {
+      showSkills(data);
+    });
+  
+    fetchData("projects").then(data => {
+      showProjects(data);
+    });
+  
+    // Example usage of filterSkills function
+    /*document.getElementById('filterButtons').addEventListener('click', function(event) {
+      if (event.target.matches('.filter-btn')) {
+        const category = event.target.dataset.category;
+        filterSkills(category);
+      }
+    });*/
   });
-
-function showProjects(projects) {
+  
+  function showProjects(projects) {
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
-    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
+    projects.slice(0, 5).forEach(project => {
+      if (project.category !== "automation") {
         projectHTML += `
-        <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>`
+          <div class="box tilt">
+            <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="${project.name}" />
+            <div class="content">
+              <div class="tag">
+                <h3>${project.name}</h3>
+              </div>
+              <div class="desc">
+                <p>${project.desc}</p>
+                <div class="btns">
+                  <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                  <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>`;
+      }
     });
     projectsContainer.innerHTML = projectHTML;
-
-    // <!-- tilt js effect starts -->
-    VanillaTilt.init(document.querySelectorAll(".tilt"), {
-        max: 15,
-    });
-    // <!-- tilt js effect ends -->
-
-    /* ===== SCROLL REVEAL ANIMATION ===== */
-    const srtop = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 1000,
-        reset: true
-    });
-
-    /* SCROLL PROJECTS */
-    srtop.reveal('.work .box', { interval: 200 });
-
-}
-
-fetchData().then(data => {
-    showSkills(data);
-});
-
-fetchData("projects").then(data => {
-    showProjects(data);
-});
+  }
 
 // <!-- tilt js effect starts -->
-VanillaTilt.init(document.querySelectorAll(".tilt"), {
+/*VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 15,
-});
+});*/
 // <!-- tilt js effect ends -->
 
 
