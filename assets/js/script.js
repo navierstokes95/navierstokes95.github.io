58dd4c93254e138cfc06415a875308bf57a4c3b0
Arif Ahmed <borndaee.wif@gmail.com>
2024-06-22 15:46:30 +0600

    dev enable, project first ?

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,7 +129,7 @@ function showProjects(projects) {
   let projectsContainer = document.querySelector("#work .box-container");
   let projectHTML = "";
   projects.slice(0, 4).forEach(project => {
-      //if (project.category !== "all") 
+      //if (project.category !== 'all') 
           projectHTML += `
         <div class="box tilt">
           <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="${project.name}" />
