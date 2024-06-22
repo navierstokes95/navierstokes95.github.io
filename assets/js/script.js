2f32f87f59130523392eefb8ea5592e42b9b03f8
Arif Ahmed <borndaee.wif@gmail.com>
2024-06-22 16:21:01 +0600

    dev delete, linked in fixed, css center

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,25 +152,6 @@ function showProjects(projects) {
   });
   projectsContainer.innerHTML = projectHTML;
 }
-/*
-// disable developer mode
-document.onkeydown = function (e) {
-  if (e.keyCode == 123) {
-      return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-      return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-      return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-      return false;
-  }
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-      return false;
-  }
-}*/
 
 // Scroll reveal animation
 const srtop = ScrollReveal({
