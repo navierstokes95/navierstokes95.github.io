3053f4e2f97dc9100669d0b7fb09b57ba081cf8a
Arif Ahmed <borndaee.wif@gmail.com>
2024-06-29 10:23:48 +0600

    contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
       event.preventDefault();
       emailjs.init("qaa0s-uOH3nIRUoaK");
 
-      emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+      emailjs.sendForm('service_vtn3g8t', 'template_contact', '#contact-form')
           .then(function (response) {
               console.log('SUCCESS!', response.status, response.text);
               document.getElementById("contact-form").reset();
